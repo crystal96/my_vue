53a6f5936858b059214dec5d471ec7dd6bf23848
liujingjing <liujingjing@singoo.cc>
2021-02-05 17:00:54 +0800

    init-01

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,16 +5,15 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue, } from 'vue-class-component';
+import { Options, Vue } from "vue-class-component";
 
 @Options({
   props: {
     msg: String
   }
 })
-
 export default class HelloWorld extends Vue {
-  msg!: string
+  msg!: string;
 }
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -5,12 +5,11 @@ const resolve = function(dir) {
 };
 
 module.exports = {
-  publicPath: "./", // 公共路径
-  outputDir: "dist", // 输出文件目录
+  publicPath: "/", // 公共路径
   // outpuoDir: process.env.NODE_ENV === "development" ? 'devdist' : 'dist', // 不同的环境打不同包名
   lintOnSave: true, // eslint-loader 是否在保存的时候检查
-  productionSourceMap: false, // 是否在构建生产包时生成sourcdeMap
-  runtimeCompiler: true,
+  productionSourceMap: false, // 是否在构建生产包时生成sourcdeMap文件
+  runtimeCompiler: true, //是否使用包含运行时编译器的 Vue 构建版本。设置true后你就可以在使用template
 
   chainWebpack: config => {
     config.optimization.runtimeChunk("single");
@@ -34,20 +33,32 @@ module.exports = {
     }
   },
 
+  // css相关配置
+  css: {
+    // 是否使用css分离插件 ExtractTextPlugin 生产环境下是true,开发环境下是false
+    extract: true,
+    // 开启 CSS source maps?
+    sourceMap: false,
+    // css预设器配置项
+    loaderOptions: {},
+    // 去掉文件名中的 .module
+    requireModuleExtension: false
+  },
   // webpack-dev-server 相关配置-开发模式
   devServer: {
     host: "0.0.0.0", // 配置host
     port: 8080, // 端口
     https: false,
     open: true, // 自动打开浏览器
-    hot: true, // 是否打开热重载模式
+    hot: true, // 热加载
     overlay: {
       // 出现编译器错误或警告时，在浏览器中显示全屏覆盖层
       warning: false, // 警告
       error: true // 错误-编译出错是显示在网页上
-    }
+    },
     // 设置反向代理-代理服务器
     // proxy: {
+    //   // 开发
     //   "/serve": {
     //     target: "http://****.****.****.****:8001", //目标接口域名
     //     secure: false, //false为http访问，true为https访问
@@ -55,28 +66,18 @@ module.exports = {
     //     pathRewrite: {
     //       "^/serve": "" //重写接口
     //     }
+    //   },
+    //   // 生产
+    //   "^/v1": {
+    //     //匹配包含 /v1的接口  如：v1/xxx/xx
+    //     target: process.env.VUE_APP_V,
+    //     ws: true, // 开启webSocket
+    //     changeOrigin: true
     //   }
-    // },
+    // }
     // before: app => {} //提供在服务器内部的其他中间件之前执行自定义中间件的能力
   },
 
-  // css相关配置
-  css: {
-    // 是否使用css分离插件 ExtractTextPlugin
-    extract: true,
-    // 开启 CSS source maps?
-    sourceMap: false,
-    // css预设器配置项
-    loaderOptions: {
-      sass: {
-        data: `@import "./src/assets/css/common.scss"`
-      }
-    },
-    // 去掉文件名中的 .module
-    requireModuleExtension: false
-  },
-  // use thread-loader for babel & TS in production build
-  // enabled by default if the machine has more than 1 cores
   parallel: require("os").cpus().length > 1,
 
   // 第三方插件配置
