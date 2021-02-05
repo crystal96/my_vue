29a5310f32ec464c25d6efba12b1f246c35530b5
liujingjing <liujingjing@singoo.cc>
2021-02-05 14:56:58 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld :msg="msgVal" />
+    <button @click="toCountFun">跳转count页面</button>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { Options, Vue} from 'vue-class-component';
+
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Options({
-  components: {
-    HelloWorld,
+  data(){
+    return {
+      // msgVal: 'Welcome to Your Vue.js + TypeScript App',
+      msgVal: '这是一个msg'
+    }
   },
+  components: {
+    HelloWorld
+  },
+  methods:{
+    toCountFun(){
+      this.$router.push('/count')
+    }
+  }
 })
+
+// @Component({
+//     props: {
+//         firstName: String,
+//         lastName: String
+//     },
+//     components: {
+//         HelloWorld,
+//     }
+// })
+
 export default class Home extends Vue {}
 </script>
---
+++ src/views/count.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button v-on:click="decrement">-</button>
+    {{ count }}
+    <button v-on:click="increment">+</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+
+    decrement() {
+      this.count--;
+    }
+  }
+})
+export default class Counter extends Vue {}
+</script>
